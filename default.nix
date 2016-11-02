c76a88b1b908237e2016b06a63c76f8f642e83c4
Rok Garbas <rok@garbas.si>
2016-11-02 20:33:05 +0100

    filter what we consider a source in default.nix

--- default.nix
+++ default.nix
@@ -4,7 +4,13 @@
 
 pkgs.rustPlatform.buildRustPackage {
   name = "nix-installer-${version}";
-  src = ./.;
+  src = builtins.filterSource
+    (path: type: baseNameOf path != "default.nix"
+              && baseNameOf path != "release.nix"
+              && baseNameOf path != "result"
+              && baseNameOf path != "target"
+              )
+    ./.;
   depsSha256 = "032328aa0s1ddkrsnddw4pxckpa8z5szfkgqq9dn9w3ai5iykrgs";
   doCheck = false;
 }
